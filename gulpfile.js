cc72b0029e758755f12d34a96d66a4f4bfecd070
dhiegohenrique <dhiego_henrique20@yahoo.com.br>
2017-07-31 20:58:56 -0300

    adicionando deploy no Heroku

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
 var jsonMinify = require('gulp-json-minify');
+var gulpCopy = require('gulp-copy');
 
 gulp.task('jsonMinify', function() {
     return gulp.src('dist/assets/i18n/*.json')
@@ -8,6 +9,15 @@ gulp.task('jsonMinify', function() {
         .pipe(gulp.dest('dist/assets/i18n'));
 });
 
+gulp.task('copyProject', function() {
+    var sources = [
+        'package.json'
+    ];
+
+    return gulp.src(sources)
+        .pipe(gulpCopy('dist', { prefix: 0 }));
+});
+
 gulp.task('default', function(callBack) {
-    return runSequence('jsonMinify', callBack);
+    return runSequence('jsonMinify', 'copyProject', callBack);
 });
