20a791c7cfc46ec94ce7529a048ed8dbd8508117
dhiegohenrique <dhiego_henrique20@yahoo.com.br>
2017-07-31 21:43:06 -0300

    adicionando deploy no Heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  // urlUsers: 'https://suntech-back.herokuapp.com/users',
-  urlUsers: 'http://localhost:3000/users',
+  urlUsers: 'https://suntech-back.herokuapp.com/users',
+  // urlUsers: 'http://localhost:3000/users',
   languages: ['en', 'es', 'pt']
 };
